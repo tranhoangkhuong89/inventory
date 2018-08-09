ff288181faa0925e67ebdc162e845a475df24ec7
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2018-08-09 16:20:12 +0700

    Update funcCB.js

--- js/funcCB.js
+++ js/funcCB.js
@@ -3,16 +3,16 @@ function combineDB(arrayBuffer) {
 	var tables = dba.prepare("SELECT * FROM sqlite_master WHERE type='table' ORDER BY name");
 	/////////
 	while (tables.step()) {
-        var rowObj = tables.getAsObject();
-        var name = rowObj.name;
-	    var query1="select * from "+name;
-	    var sel;
-	    try {
-		sel = dba.prepare(query1);
-	    } 
-	    catch (ex) {
-		showError(ex);
-	    }
+		var rowObj = tables.getAsObject();
+		var name = rowObj.name;
+		var query1="select * from '"+name+"'";
+		var sel;
+		try {
+			sel = dba.prepare(query1);
+		    } 
+		catch (ex) {
+			showError(ex);
+		    }
 
 		while (sel.step()) {
 			var s = sel.get();
@@ -24,7 +24,7 @@ function combineDB(arrayBuffer) {
 			}
 			query2=query2.replace(/.$/,")")
 			try {
-				sel = db.run(query2);
+				db.run(query2);
 			} 
 			catch (ex) {
 				showError(ex);
