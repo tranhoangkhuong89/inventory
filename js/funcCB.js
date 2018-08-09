c93ed01ec0b19131c65ea3d07eff2329b5433c27
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2018-08-09 16:37:01 +0700

    Update funcCB.js

--- js/funcCB.js
+++ js/funcCB.js
@@ -1,8 +1,8 @@
 function combineDB(arrayBuffer) {
 	var max=db.prepare("SELECT max(id) FROM 'item' GROUP BY id");
-	var s;
+	var s_count;
 	while (max.step()) {
-			s += max.get();
+			s_count = max.get();
 	}
 	var dba = new SQL.Database(new Uint8Array(arrayBuffer));
 	var tables = dba.prepare("SELECT * FROM sqlite_master WHERE type='table' ORDER BY name");
@@ -22,7 +22,7 @@ function combineDB(arrayBuffer) {
 		while (sel.step()) {
 			var s = sel.get();
 		
-			var query2 = "INSERT INTO "+name+" values("+(maxx++)+",";
+			var query2 = "INSERT INTO "+name+" values("+(s_count++)+",";
 		
 			for (var i = 1; i < s.length; i++) {
 					  query2+="'"+s[i]+"',";
