64826824e62e2ed8858c52353291b88a108051f6
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2018-08-21 10:36:34 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ dbx.filesListFolder({path: ur})
 			tableList.select2("val", list_url[0]);
 			//doDefaultSelect(list_url[0]);
 
-			$(".chosen-select").chosen();
+			$(".chosen-select").chosen({width: "95%"});
 			$("#output-box").fadeIn();
 			$(".nouploadinfo").hide();
 			$("#sample-db-link").hide();
